ea7e94e41c2faadc256a1734b015580c27c6bcfd
Blessing <blessingt2756@gmail.com>
2024-10-08 17:02:00 +0200

    semi colon delimiter modification

--- scripts/comDelim.py
+++ scripts/comDelim.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Load the CSV file
-input_file = 'your_input_file.csv'  # Replace with your actual file path
-output_file = 'your_output_file.csv'  # Replace with your desired output file path
+input_file = '/Users/blessingt03/Documents/GitHub/rProgrammingCaptsone/spreadsheets/cleaned_listings_updated.csv'  # Replace with your actual file path
+output_file = '/Users/blessingt03/Documents/GitHub/rProgrammingCaptsone/spreadsheets/cleaned_listings_wSemiCol.csv'  # Replace with your desired output file path
 
 # Read the CSV file with commas as the delimiter
 df = pd.read_csv(input_file)
