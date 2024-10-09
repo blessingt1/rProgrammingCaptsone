84573d1583d866366a9d64b010d9cc549dedc3fe
Blessing <blessingt2756@gmail.com>
2024-10-09 16:43:41 +0200

    updated listing

--- scripts/comDelim.py
+++ scripts/comDelim.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Load the CSV file
-input_file = '/Users/milanichikeka/Documents/GitHub/rProgrammingCaptsone/spreadsheets/updated_location_without_commas.csv'  # Replace with your actual file path
-output_file = '/Users/milanichikeka/Documents/GitHub/rProgrammingCaptsone/sqlTables/updated_location_with_commas.csv'  # Replace with your desired output file path
+input_file = '/Users/blessingt03/Documents/GitHub/rProgrammingCaptsone/spreadsheets/listingSpread.csv'  # Replace with your actual file path
+output_file = '/Users/blessingt03/Documents/GitHub/rProgrammingCaptsone/sqlTables/listingTable.csv'  # Replace with your desired output file path
 
 # Read the CSV file with commas as the delimiter
 df = pd.read_csv(input_file)
